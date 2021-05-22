f1f16ab62d630ae1b224bee2427e2ac251f72e26
souravSarkar78 <ss830897@gmail.com>
2021-05-22 18:03:29 +0530

    commit

--- Train.py
+++ Train.py
@@ -1,8 +1,15 @@
 import os 
 import tensorflow as tf
+import time
 
 print(tf.__version__)
 
+def give_space(n=2):
+    for i in range(n):
+        print(" ")
+
+if not os.path.exists("Tensorflow"):
+    os.mkdir("Tensorflow")
 
 CUSTOM_MODEL_NAME = 'my_ssd_mobnet' 
 PRETRAINED_MODEL_NAME = 'ssd_mobilenet_v2_fpnlite_320x320_coco17_tpu-8'
@@ -17,9 +24,9 @@ paths = {
     'SCRIPTS_PATH': os.path.join('Tensorflow','scripts'),
     'APIMODEL_PATH': os.path.join('Tensorflow','models'),
     'ANNOTATION_PATH': os.path.join('Tensorflow', 'workspace','annotations'),
-    'IMAGE_PATH': os.path.join('Tensorflow', 'workspace','images'),
-    'TEST_IMAGE_PATH': os.path.join('Tensorflow', 'workspace','images', 'test'),
-    'TRAIN_IMAGE_PATH': os.path.join('Tensorflow', 'workspace','images', 'train'),
+    'IMAGE_PATH': os.path.join('images'),
+    'TEST_IMAGE_PATH': os.path.join('images', 'test'),
+    'TRAIN_IMAGE_PATH': os.path.join('images', 'train'),
     'MODEL_PATH': os.path.join('Tensorflow', 'workspace','models'),
     'PRETRAINED_MODEL_PATH': os.path.join('Tensorflow', 'workspace','pre-trained-models'),
     'CHECKPOINT_PATH': os.path.join('Tensorflow', 'workspace','models',CUSTOM_MODEL_NAME), 
@@ -39,28 +46,52 @@ for path in paths.values():
     if not os.path.exists(path):
         os.mkdir(path)
 
+
 clone_command = "git clone https://github.com/tensorflow/models "+paths['APIMODEL_PATH']
 
 
 if not os.path.exists(os.path.join(paths['APIMODEL_PATH'], 'research', 'object_detection')):
+    print("Downloading Tensorfloe Object-detection-API")
     os.system(clone_command)
 else:
     print("Tensorflow Object Detection Model found at",paths['APIMODEL_PATH'])
-#os.system('cd Tensorflow/models/research && protoc object_detection/protos/*.proto --python_out=. && cp object_detection/packages/tf2/setup.py . && python -m pip install . ')
+
+give_space()
+
+protobuff_command = 'cd Tensorflow/models/research && protoc object_detection/protos/*.proto --python_out=. && cp object_detection/packages/tf2/setup.py . && python -m pip install .'
+
+os.system(protobuff_command)
+    
+print("Protobuff Copied !!!!!!!")
+
+give_space()
+
+
+print("Verifing System.....")
+give_space(1)
+#############os.system('cd Tensorflow/models/research && protoc object_detection/protos/*.proto --python_out=. && cp object_detection/packages/tf2/setup.py . && python -m pip install . ')
 VERIFICATION_SCRIPT = "python3.7 " +os.path.join(paths['APIMODEL_PATH'], 'research', 'object_detection', 'builders', 'model_builder_tf2_test.py')
 # Verify Installation
-#os.system(VERIFICATION_SCRIPT)
+os.system(VERIFICATION_SCRIPT)
+
+print("System Verified Succesfully....")
+
 
 import object_detection
+
+give_space()
+print("Downloading Pretrained Model....")
 url= ('wget '+PRETRAINED_MODEL_URL)
-#os.system(url)
+os.system(url)
+
+print("Succesfully Downloaded Pretrained Model..")
 
 move_command = 'mv '+PRETRAINED_MODEL_NAME+'.tar.gz '+paths['PRETRAINED_MODEL_PATH']
-#os.system(move_command)
+os.system(move_command)
 
 unzip_command = 'cd '+paths['PRETRAINED_MODEL_PATH']+ ' && tar -zxvf ' +PRETRAINED_MODEL_NAME+'.tar.gz'
 
-#os.system(unzip_command)
+os.system(unzip_command)
 
 labels = [{'name':'car', 'id':1}, {'name':'people', 'id':2}, {'name':'rickshaw', 'id':3}, {'name':'animal', 'id':4},{'name':'bike', 'id':5}, {'name':'cycle', 'id':6}, {'name':'red signal', 'id':7}, {'name':'green signal', 'id':8}, {'name':'yellow signal', 'id':9}]
 
@@ -71,3 +102,70 @@ with open(files['LABELMAP'], 'w') as f:
         f.write('\tid:{}\n'.format(label['id']))
         f.write('}\n')
     print('Labelmap Created done')
+
+give_space()
+
+if not os.path.exists(files['TF_RECORD_SCRIPT']):
+    print("Downloading TF_Record Script.....")
+    git_link_command = 'git clone https://github.com/nicknochnack/GenerateTFRecord '+paths['SCRIPTS_PATH']
+    os.system(git_link_command)
+
+    print("TF_Record Script Downloaded.................................")
+
+give_space()
+print("Generating TF_Record...........")
+tf_record_train = 'python '+files['TF_RECORD_SCRIPT']+' -x '+paths['TRAIN_IMAGE_PATH']+' -l '+files['LABELMAP']+' -o '+os.path.join(paths['ANNOTATION_PATH'], 'train.record') 
+tf_record_test = 'python '+files['TF_RECORD_SCRIPT']+' -x '+paths['TEST_IMAGE_PATH']+' -l '+files['LABELMAP']+' -o '+os.path.join(paths['ANNOTATION_PATH'], 'test.record') 
+os.system(tf_record_train)
+os.system(tf_record_test)
+print("TF_Record Generated succsfully ...........")
+
+give_space()
+
+
+if os.name =='posix':
+    cp_command = 'cp '+os.path.join(paths['PRETRAINED_MODEL_PATH'], PRETRAINED_MODEL_NAME, 'pipeline.config')+' '+os.path.join(paths['CHECKPOINT_PATH'])
+    os.system(cp_command)
+
+print("Pipeline config Copied")
+time.sleep(2)
+give_space()
+
+from object_detection.utils import config_util
+from object_detection.protos import pipeline_pb2
+from google.protobuf import text_format
+
+print("Modifing config file....")
+
+config = config_util.get_configs_from_pipeline_file(files['PIPELINE_CONFIG'])
+
+pipeline_config = pipeline_pb2.TrainEvalPipelineConfig()
+with tf.io.gfile.GFile(files['PIPELINE_CONFIG'], "r") as f:                                                                                                                                                                                                                     
+    proto_str = f.read()                                                                                                                                                                                                                                          
+    text_format.Merge(proto_str, pipeline_config)  
+
+pipeline_config.model.ssd.num_classes = len(labels)
+pipeline_config.train_config.batch_size = 4
+pipeline_config.train_config.fine_tune_checkpoint = os.path.join(paths['PRETRAINED_MODEL_PATH'], PRETRAINED_MODEL_NAME, 'checkpoint', 'ckpt-0')
+pipeline_config.train_config.fine_tune_checkpoint_type = "detection"
+pipeline_config.train_input_reader.label_map_path= files['LABELMAP']
+pipeline_config.train_input_reader.tf_record_input_reader.input_path[:] = [os.path.join(paths['ANNOTATION_PATH'], 'train.record')]
+pipeline_config.eval_input_reader[0].label_map_path = files['LABELMAP']
+pipeline_config.eval_input_reader[0].tf_record_input_reader.input_path[:] = [os.path.join(paths['ANNOTATION_PATH'], 'test.record')]
+
+
+config_text = text_format.MessageToString(pipeline_config)                                                                                                                                                                                                        
+with tf.io.gfile.GFile(files['PIPELINE_CONFIG'], "wb") as f:                                                                                                                                                                                                                     
+    f.write(config_text)  
+
+print("Config modified..")
+give_space()
+
+print("Hold on ... Training is Starting....")
+give_space()
+
+TRAINING_SCRIPT = os.path.join(paths['APIMODEL_PATH'], 'research', 'object_detection', 'model_main_tf2.py')
+
+command = "python {} --model_dir={} --pipeline_config_path={} --num_train_steps=2000".format(TRAINING_SCRIPT, paths['CHECKPOINT_PATH'],files['PIPELINE_CONFIG'])
+give_space()
+os.system(command)
